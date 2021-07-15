bfbbfc3d830296341a0f2bd1b085a0d0b44d0943
MUNIRAJA KONETI <muniraja.k1444@gmail.com>
2021-07-15 14:38:41 +0700

    atm programe

--- koneti.py
+++ koneti.py
@@ -1,2 +1,7 @@
-print("Hey ,This is muniraja koneti .And i am from mallemala.If you have any dooubt regarding python please consult me once .")
-'''hey ,this is muniraja koneti,,,,,,,,,,,,,,,,,'''
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="muniraja@256")
+mycursor=mydb.cursor()
+
+mycursor.execute('show databases')
+
+
